4c2346e57da10f1ca157c0bb74f7364b5529ee54
xyaman <dbarriapaiva@gmail.com>
2025-06-26 17:10:38 +0900

    feat(nvim): add slimline plugin

---
+++ nvim/.config/nvim/lua/plugins/slimline.lua
@@ -0,0 +1,20 @@
+return {
+    "sschleemilch/slimline.nvim",
+    opts = {
+        style = "fg",
+        components = {
+            left = { "mode", "path" },
+            right = { "diagnostics", "filetype_lsp", "progress" },
+        },
+        configs = {
+            diagnostics = {
+                workspace = true,
+            },
+            filetype_lsp = {
+                hl = {
+                    secondary = "Normal",
+                },
+            },
+        },
+    },
+}
