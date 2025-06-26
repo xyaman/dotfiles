f626be045783fe9d141815100eea3255c247eff0
xyaman <dbarriapaiva@gmail.com>
2025-06-26 18:15:04 +0900

    feat(nvim): load blink.nvim on `CmdlineEnter`

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -2,7 +2,7 @@ return {
     "saghen/blink.cmp",
     -- optional: provides snippets for the snippet source
     dependencies = { "rafamadriz/friendly-snippets" },
-    event = "InsertEnter",
+    event = { "InsertEnter", "CmdlineEnter" },
 
     -- use a release tag to download pre-built binaries
     version = "1.*",
