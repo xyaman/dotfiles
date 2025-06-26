55ce5fd53aa3702fbb86a57f9496c0b6816dacda
xyaman <dbarriapaiva@gmail.com>
2025-06-26 13:12:47 +0900

    feat(theme): use vague.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -24,8 +24,8 @@ require("lazy").setup("plugins", {
     rocks = { enabled = false },
 })
 
--- vim.cmd("colorscheme rose-pine")
-vim.cmd.colorscheme("oh-lucy-evening")
+vim.cmd.colorscheme("oh-lucy-evening") -- If i dont do this first, vague is not being loaded, idk why
+vim.cmd.colorscheme("vague")
 
 -- set treesitter context highlight
 vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "#6D5978" })
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,14 @@
 return {
     "Yazeed1s/oh-lucy.nvim",
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        opts = {
-            disable_background = true,
-            extend_background_behind_borders = true,
-            dim_inactive_background = false,
-            styles = {
-                transparency = true,
-            },
-            highlight_groups = {
-                TelescopeBorder = { fg = "highlight_high", bg = "none" },
-                TelescopeNormal = { bg = "none" },
-                TelescopePromptNormal = { bg = "none" },
-                TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-                TelescopeSelection = { fg = "text", bg = "none" },
-                TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-            },
-        },
+        "vague2k/vague.nvim",
+        priority = 1000,
+        config = function()
+            require("vague").setup({
+                transparent = true,
+            })
+
+            vim.cmd("colorscheme vague")
+        end,
     },
 }
