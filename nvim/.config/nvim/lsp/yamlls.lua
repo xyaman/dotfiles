c04e256e02048e2ef0a766616b9ee1fbd02fb816
xyaman <dbarriapaiva@gmail.com>
2025-06-27 15:41:00 +0900

    feat(nvim): add schemastore + related lsp
    - add json-lsp and yaml-lsp

---
+++ nvim/.config/nvim/lsp/yamlls.lua
@@ -0,0 +1,16 @@
+---@type vim.lsp.Config
+return {
+    cmd = { "yaml-language-server", "--stdio" },
+
+    filetypes = { "yaml" },
+
+    settings = {
+        yaml = {
+
+            -- Using the schemastore plugin for schemas.
+            schemastore = { enable = false, url = "" },
+
+            schemas = require("schemastore").yaml.schemas(),
+        },
+    },
+}
