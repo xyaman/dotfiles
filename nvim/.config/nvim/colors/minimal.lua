01c24ccfef1d00a84cdd5ba99198df6a141ced3f
xyaman <dbarriapaiva@gmail.com>
2025-07-10 08:23:02 +0900

    feat(nvim): add update theme

--- nvim/.config/nvim/colors/minimal.lua
+++ nvim/.config/nvim/colors/minimal.lua
@@ -1,20 +1,20 @@
 require("mini.base16").setup({
     palette = {
-        base00 = "#151515",
-        base01 = "#202020",
-        base02 = "#303030",
-        base03 = "#707070",
-        base04 = "#b0b0b0",
-        base05 = "#d0d0d0",
-        base06 = "#e0e0e0",
-        base07 = "#f5f5f5",
-        base08 = "#fb9fb1",
-        base09 = "#eda987",
-        base0A = "#ddb26f",
-        base0B = "#acc267",
-        base0C = "#12cfc0",
-        base0D = "#6fc2ef",
-        base0E = "#e1a3ee",
-        base0F = "#deaf8f",
+        base00 = "#181818", -- background (gray)
+        base01 = "#232323", -- lighter background (gray)
+        base02 = "#2e2e2e", -- selection background (gray)
+        base03 = "#444444", -- comments, invisibles (gray)
+        base04 = "#b0b0b0", -- dark foreground (gray)
+        base05 = "#d0d0d0", -- default foreground (gray)
+        base06 = "#e0e0e0", -- light foreground (gray)
+        base07 = "#f5f5f5", -- lightest foreground (gray)
+        base08 = "#e06c75", -- red (accent 1)
+        base09 = "#e5c07b", -- yellow/orange (accent 2)
+        base0A = "#c6c6c6", -- white 2
+        base0B = "#e5c07b", -- yellow
+        base0C = "#888888", -- gray
+        base0D = "#888888", -- gray
+        base0E = "#888888", -- gray
+        base0F = "#888888", -- gray
     },
 })
