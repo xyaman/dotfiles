7fe4dc68bc54222a44a4276aea0ba06c634ff21c
xyaman <dbarriapaiva@gmail.com>
2026-07-07 15:12:40 +0900

    fix(nvim): gitsigns preview_hunk keymap shadowed prev_hunk

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -7,7 +7,7 @@ return {
         { "<leader>hu", "<cmd>lua require'gitsigns'.undo_stage_hunk()<CR>", "Undo stage hunk" },
         { "<leader>hr", "<cmd>lua require'gitsigns'.reset_hunk()<CR>", "Reset hunk" },
         { "<leader>hR", "<cmd>lua require'gitsigns'.reset_buffer()<CR>", "Reset buffer" },
-        { "<leader>hp", "<cmd>lua require'gitsigns'.preview_hunk()<CR>", "Preview hunk" },
+        { "<leader>hv", "<cmd>lua require'gitsigns'.preview_hunk()<CR>", "Preview hunk" },
         { "<leader>hb", "<cmd>lua require'gitsigns'.blame_line()<CR>", "Blame line" },
         { "<leader>hS", "<cmd>lua require'gitsigns'.stage_buffer()<CR>", "Stage buffer" },
         { "<leader>hU", "<cmd>lua require'gitsigns'.reset_buffer_index()<CR>", "Reset buffer index" },
